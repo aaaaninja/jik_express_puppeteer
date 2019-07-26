786e17c67a9cddec6bda245a8d07c56dd12c8272
k1nPr45b0 <39687282+k1nPr45b0@users.noreply.github.com>
2019-07-26 11:24:20 +0900

    :zap: なんか早くなるらしいoptions
    >https://qiita.com/markey/items/ebf2b48626b6ac61ee89
    
    ついでにこれにするとssh x11 forwardingができるようになった on windows
    ```sh
    ssh -XC -l aaaa b
    ```

--- piyo.js
+++ piyo.js
@@ -10,7 +10,17 @@ const urls = [ "https://github.com/GoogleChrome/puppeteer/blob/master/README.md"
              , "https://github.com/GoogleChrome/puppeteer"
              ];
 
-const browser = await pp.launch({args: ['--no-sandbox','--disable-setuid-sandbox'], headless: false}); // debug用途にheadlessをfalseにしてgui表示させてる.
+const browser = await pp.launch({
+  args: [ '--disable-gpu'
+  | ¦ ┆ , '--disable-dev-shm-usage'
+  | ¦ ┆ , '--disable-setuid-sandbox'
+  | ¦ ┆ , '--no-first-run'
+  | ¦ ┆ , '--no-sandbox'
+  | ¦ ┆ , '--no-zygote'
+  | ¦ ┆ , '--single-process'
+  | ¦ ┆ ]
+, headless: false
+}); // debug用途にheadlessをfalseにしてgui表示させてる.
 const tabs    = await Promise.all([...Array(100)].map( (_, i) => browser.newPage() )); // テキトーに100枚バっとtabを開いてる.
 await Promise.all(urls.map( (url, idx) => tabs[idx].goto(url))); // すべて並列で進むが、次に行に行くのは一番遅い読み込みのやつに引っ張られる.
 await Promise.all(urls.map( (url, idx) => tabs[idx].screenshot({path: `${idx}_oo.png`})));
